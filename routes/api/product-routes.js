539c605dc70ea2c13fcf5e87e99c1f9c5a705aab
Fancy Pants <edaily94@gmail.com>
2023-04-19 20:25:21 -0700

    I forgot the delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,6 +111,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((deletedProduct) => {
+    res.status(200).json({message: 'Tag deleted successfully.'});
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
